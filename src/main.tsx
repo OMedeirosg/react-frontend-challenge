bad0215070a624c20b0565a9b488e4aff1e0de0c
Gabriel Medeiros <gabrielmed00@hotmail.com>
2026-03-28 18:38:23 -0300

    feat: add files routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+
 import { ReactQueryProvider } from './providers/ReactQueryProvider.tsx'
+import { RouterProvider, createRouter } from '@tanstack/react-router'
+import { routeTree } from './routeTree.gen'
+
+const router = createRouter({ routeTree })
+
+
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ReactQueryProvider>
-      <App />
+      <RouterProvider router={router} />
     </ReactQueryProvider>
   </StrictMode>,
 )
